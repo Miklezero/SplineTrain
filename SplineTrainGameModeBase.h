62e56639602c1ce1840ab46dad0ea20dfc61204a
Mikhail <umzero@yandex.ru>
2022-12-20 08:58:08 +0300

    Created a new SplineTrain project

---
+++ SplineTrainGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SplineTrainGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPLINETRAIN_API ASplineTrainGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
